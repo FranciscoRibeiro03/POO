1765ea0a435440965d85f789fe0c8039249532d4
Francisco Ribeiro <francisco.ribeiro@ua.pt>
2022-03-14 23:34:26 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# POO (Object Oriented Programming)
+
+This repository will contain exercises for the course [POO](https://www.ua.pt/en/uc/12279) (Programação Orientada a Objetos, which means Object Oriented Programming, in Portuguese).
+
+## Folder Structure
+
+The workspace contains two folders, where:
+
+- `src` is the folder to maintain sources
+- `lib` is the folder to maintain dependencies
---
+++ src/Game.java
@@ -0,0 +1,26 @@
+import java.util.Random;
+import java.util.Scanner;
+
+// Create a number guessing game that uses a random number generator to generate a number between 1 and 100.
+// The user should be able to guess the number and the program should tell them whether they guessed too high, too low, or correct.
+
+public class Game {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int randomNumber = rand.nextInt(100) + 1;
+        System.out.println("I'm thinking of a number between 1 and 100.");
+        System.out.println("Can you guess what it is?");
+        Scanner input = new Scanner(System.in);
+        int guess = input.nextInt();
+        while (guess != randomNumber) {
+            if (guess < randomNumber) {
+                System.out.println("Too low!");
+            } else {
+                System.out.println("Too high!");
+            }
+            System.out.println("Guess again: ");
+            guess = input.nextInt();
+        }
+        System.out.println("You got it! The number was " + randomNumber);
+    }
+}
