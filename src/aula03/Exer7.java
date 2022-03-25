77cfa292b9d31460c8a248f03b9e447707c47164
Guilherme Vieira <guilhermevieira@ua.pt>
2022-03-25 10:44:42 +0000

    Corrigir bem o exercício 7

--- src/aula03/Exer7.java
+++ src/aula03/Exer7.java
@@ -6,29 +6,31 @@ import java.util.Random;
 public class Exer7 {
 
 	public static void main(String[] args) {
-		int randomNumber = new Random().nextInt(100) + 1;
-		
 		Scanner sc = new Scanner(System.in);
-
-		int attempts = 0;
 		String answer;
 
 		do {
-
-			int guess = Util.getInt("Introduza um número entre 0 e 100: ", sc, 0, 100);
-			attempts++;
-
-			if (guess == randomNumber) break;
-			else if (guess > randomNumber) System.out.println("O número secreto é menor do que o número introduzido!");
-			else System.out.println("O número secreto é maior do que o número introduzido!");
-
+			game(sc);
 			answer = Util.getString("Deseja continuar? Escreva (S)im para continuar, ou outra coisa para sair: ", sc).toLowerCase();
-
 		} while (answer.equals("s") || answer.equals("sim"));
 
+		sc.close();
+	}
+
+	public static void game(Scanner sc) {
+		int attempts = 0, guess;
+		int randomNumber = new Random().nextInt(100) + 1;
+
+		do {
+			guess = Util.getInt("Introduza um número: ", sc, 0, 100);
+			attempts++;
+
+			if (guess > randomNumber) System.out.println("O número secreto é menor do que o número introduzido!");
+			else if (guess < randomNumber) System.out.println("O número secreto é maior do que o número introduzido!");
+		} while (guess != randomNumber);
+
+		System.out.print('\n');
 		System.out.println("O número secreto era " + randomNumber + "!");
 		System.out.println("Tentativas: " + attempts);
-
-		sc.close();
 	}
 }
