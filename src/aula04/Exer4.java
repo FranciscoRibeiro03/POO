c11a06b3acebfee258042fa94306176b2b56e632
Guilherme Vieira <guilhermevieira@ua.pt>
2022-03-28 16:43:14 +0100

    Tabela do Exer 4

--- src/aula04/Exer4.java
+++ src/aula04/Exer4.java
@@ -47,7 +47,42 @@ public class Exer4 {
     }
 
     public static void drawCalendar(int month, int year, int weekDay) {
+		int days = numberOfDaysInMonth(month, year);
+		String monthName = getMonthName(month);
 
+		int monthNameLength = monthName.length();
+		int yearLength = String.valueOf(year).length();
+		int padding = (20 - monthNameLength - yearLength) / 2;
+		String paddingString = "";
+		for (int i = 0; i < padding; i++) paddingString += " ";
+		
+		System.out.println(paddingString + monthName + " " + year + paddingString);
+		System.out.println("Su Mo Tu We Th Fr Sa");
+
+		for (int i = 0; i < weekDay; i++) System.out.print("   ");
+
+		for (int i = 1; i <= days; i++) {
+			System.out.printf("%2d ", i);
+			if ((i + weekDay) % 7 == 0 || i == days) System.out.println();
+		}
     }
+
+	public static String getMonthName(int month) {
+		switch (month) {
+			case 1: return "January";
+			case 2: return "February";
+			case 3: return "March";
+			case 4: return "April";
+			case 5: return "May";
+			case 6: return "June";
+			case 7: return "July";
+			case 8: return "August";
+			case 9: return "September";
+			case 10: return "October";
+			case 11: return "November";
+			case 12: return "December";
+			default: return "";
+		}
+	}
     
 }
